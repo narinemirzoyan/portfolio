29d520014a328849b576dfe6a5f38df8ca95ec76
Khachatur Gevorgyan <khachatur.gevorgyan.1993@gmail.com>
2024-06-24 01:08:05 +0400

    Portfolio

---
+++ precache-manifest.24af7a17fad9c5dfc185ec827de1b914.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b33d6fed962ae3db0b2dcbf31dbf7b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c54984bdd3f6419500e0",
+    "url": "/portfolio/static/css/main.c38196ad.chunk.css"
+  },
+  {
+    "revision": "ccaadebdad4c9d07eb68",
+    "url": "/portfolio/static/js/2.b4c5cb25.chunk.js"
+  },
+  {
+    "revision": "c54984bdd3f6419500e0",
+    "url": "/portfolio/static/js/main.9f34b4cc.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5b07845ba0b91ee8590280b75b73ad97",
+    "url": "/portfolio/static/media/01-big.5b07845b.jpg"
+  },
+  {
+    "revision": "717fd02a22dab48eb8e990f65cc07cbe",
+    "url": "/portfolio/static/media/01.717fd02a.jpg"
+  },
+  {
+    "revision": "5b7ec576e127526f07c1470cd6b1f1ba",
+    "url": "/portfolio/static/media/02-big.5b7ec576.jpg"
+  },
+  {
+    "revision": "8bcc0de25fa82cfd6e6700a9f67868f2",
+    "url": "/portfolio/static/media/02.8bcc0de2.jpg"
+  },
+  {
+    "revision": "600ad6edb9b1c7e4fdd690d02a4aeb65",
+    "url": "/portfolio/static/media/03-big.600ad6ed.jpg"
+  },
+  {
+    "revision": "0305fa5e28ebabc868dfd0ff5165b01f",
+    "url": "/portfolio/static/media/03.0305fa5e.jpg"
+  },
+  {
+    "revision": "f2fb53b98c1138b780e4dc896e8f2826",
+    "url": "/portfolio/static/media/04-big.f2fb53b9.jpg"
+  },
+  {
+    "revision": "69d40235c89b1d2125774bc4091ed982",
+    "url": "/portfolio/static/media/04.69d40235.jpg"
+  },
+  {
+    "revision": "88abad61bc4c90a61df59ce87104dd49",
+    "url": "/portfolio/static/media/05-big.88abad61.jpg"
+  },
+  {
+    "revision": "5123a65a3af10610ed2e0205a5e79333",
+    "url": "/portfolio/static/media/05.5123a65a.jpg"
+  },
+  {
+    "revision": "78b6aea96bcf4bc0ed55652ccea3ac53",
+    "url": "/portfolio/static/media/06-big.78b6aea9.jpg"
+  },
+  {
+    "revision": "36c34e5399e4d45aa8823799dc4accb5",
+    "url": "/portfolio/static/media/06.36c34e53.jpg"
+  },
+  {
+    "revision": "3b0e25a4729ec1e94cd86bd374448a0d",
+    "url": "/portfolio/static/media/CV_Narine_Mirzoyan.3b0e25a4.pdf"
+  },
+  {
+    "revision": "63797490e90e125fd64d575658d9eac4",
+    "url": "/portfolio/static/media/email-dark.63797490.svg"
+  },
+  {
+    "revision": "1a51f2f49367223307783e71f4135c19",
+    "url": "/portfolio/static/media/email-light.1a51f2f4.svg"
+  },
+  {
+    "revision": "6f3e199351c1a9e63fa72c93d9a8386f",
+    "url": "/portfolio/static/media/header-bg-dark.6f3e1993.png"
+  },
+  {
+    "revision": "4c03563b49eb38eaffb58d98ecb75358",
+    "url": "/portfolio/static/media/header-bg-light.4c03563b.png"
+  },
+  {
+    "revision": "c3b2cb75c04bf26a5f5cdc3f7c03e86b",
+    "url": "/portfolio/static/media/linkedin-dark.c3b2cb75.svg"
+  },
+  {
+    "revision": "9bb29cc389ac011fa6f9c28ee5e2773f",
+    "url": "/portfolio/static/media/linkedin-light.9bb29cc3.svg"
+  },
+  {
+    "revision": "c0e1ca3d6c5df5bd7dbad48c0f1d5cb2",
+    "url": "/portfolio/static/media/moon.c0e1ca3d.svg"
+  },
+  {
+    "revision": "57159895b79f1da46ae839c8291ce05d",
+    "url": "/portfolio/static/media/phone-dark.57159895.svg"
+  },
+  {
+    "revision": "ea1e09054b2df7a607ca67b4d6e09a45",
+    "url": "/portfolio/static/media/phone-light.ea1e0905.svg"
+  },
+  {
+    "revision": "ccaa7d239372739f1894437fc06843a9",
+    "url": "/portfolio/static/media/sun.ccaa7d23.svg"
+  }
+]);
